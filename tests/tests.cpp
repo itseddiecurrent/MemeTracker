4b84937bc742ea73eac956c4f67c1de0d584be55
Edward Tang <qianfut2@linux-40.ews.illinois.edu>
2020-12-04 16:39:49 -0600

    WHY WASN'T THE TEST ADDED?

---
+++ tests/tests.cpp
@@ -0,0 +1,18 @@
+#include "../cs225/catch/catch.hpp"
+#include "../Graph.h"
+#include "../edge.h"
+#include <vector>
+#include <map>
+#include <iostream>
+
+TEST_CASE("Graph::getNumVertices returns the correct number of vertices") {
+    Graph g(true, false);
+    Vertex v1 = (Vertex)"1";
+
+    g.insertVertex(v1);
+    g.insertVertex("2");
+    REQUIRE(g.getNumVertices() == 2);
+}
+
+
+
